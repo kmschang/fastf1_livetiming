361078752da0395ddb93f78a52cab97f7f1f852e
kscardinal <kschang@cardinalhs.net>
2025-09-22 10:40:22 -0400

    Decoding TimingData

--- fastf1_livetiming.py
+++ fastf1_livetiming.py
@@ -3,6 +3,8 @@ import json
 import re
 from icecream import ic
 
+landoNorris = []
+
 def parse_line(f):
     line = f.readline()
     if not line: 
@@ -19,30 +21,55 @@ def parse_line(f):
     ic(payloadTimestamp)
 
     if payloadType == "TimingData":
-        payloadDataNumbers = re.findall(r"\d+", json.dumps(payloadData))
-        if len(payloadDataNumbers) >= 4:
-            driverNumber, sector, sectorSegment, segmentStatus = map(int, payloadDataNumbers[:4])
-            return {
-                "type": payloadType,
-                "timestamp": payloadTimestamp,
-                "driverNumber": driverNumber,
-                "sector": sector + 1,
-                "sectorSegment": sectorSegment + 1,
-                "segmentStatus": segmentStatus
-            }
 
-    # fallback if not TimingData or missing numbers
-    return {
-        "type": payloadType,
-        "timestamp": payloadTimestamp,
-        "data": payloadData
-    }
+        driverNumber = None
+        sector = None
+        sectorSegment = None
+        segmentStatus = None
+        previousValue = None
+        
+        # Gets the data from the drivers
+        drivers = payloadData.get("Lines", {})
+        for driverNumberStr, driverData in drivers.items():
+            driverNumber = int(driverNumberStr)
+            
+            # Gets the data from the sectprs
+            sectors = driverData.get("Sectors", {})
+            for sectorStr, sectorData in sectors.items():
+                sector = int(sectorStr)
 
+                # Gets the data from the segments
+                segments = sectorData.get("Segments", {})
+                for segmentStr, segmentData in segments.items():
+                    sectorSegment = int(segmentStr)
+
+                    # Gets the data from the status
+                    status = segmentData.get("Status")
+                    if status is not None:
+                        segmentStatus = int(status)
+
+                # Gets the data from the previousValue
+                previousValue = sectorData.get("PreviousValue")  # None if missing
+                if previousValue is not None:
+                    previousValue = float(previousValue)
+                
+            # Prints Data
+            ic(driverNumber)
+            ic(sector)
+            ic(sectorSegment)
+            ic(segmentStatus)
+            ic(previousValue)
+
+
+
+    else:
+        ic("N/A")
+            
+
+with open("driverInformation.json", "r") as f:
+    driverInfo = json.load(f)
 
 with open("cache.txt", "r") as f:
     for i in range(10):
-        result = parse_line(f)
-        if result is None: 
-            ic("Error reading file. Could be the end.")
-            break
-        ic(i, result)
+        parse_line(f)
+        print("\n")
